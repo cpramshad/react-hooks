8d1968dfac795270f19d363b74c3c969f79f1c9f
cpramshad <cpramshad@gmail.com>
2021-03-25 16:06:24 +0400

    Using useCallback hook

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,25 +7,29 @@ import Search from './Search';
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
-  useEffect(() => {
-    fetch('https://react-hooks-6ee32-default-rtdb.firebaseio.com/ingredients.json')
-    .then(response => response.json())
-    .then(resposeData => {
-      const loadedIngredients = [];
-      for(const key in resposeData) {
-        loadedIngredients.push({
-          id: key,
-          title: resposeData[key].title,
-          amount: resposeData[key].amount,
-        });
-      }
-      setUserIngredients(loadedIngredients);
-    });
-  }, []);
+  // useEffect(() => {
+  //   fetch('https://react-hooks-6ee32-default-rtdb.firebaseio.com/ingredients.json')
+  //   .then(response => response.json())
+  //   .then(resposeData => {
+  //     const loadedIngredients = [];
+  //     for(const key in resposeData) {
+  //       loadedIngredients.push({
+  //         id: key,
+  //         title: resposeData[key].title,
+  //         amount: resposeData[key].amount,
+  //       });
+  //     }
+  //     setUserIngredients(loadedIngredients);
+  //   });
+  // }, []);
 
   useEffect(() => {
     console.log('Rendering Ingredients');
-  })
+  });
+
+  const filteredIngredientsHandler = useCallback(filteredIngredients => {
+    setUserIngredients(filteredIngredients);
+  }, []);
 
 
   const addIngredientHandler = ingredient => {
@@ -54,7 +58,7 @@ const Ingredients = () => {
       <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
-        <Search />
+        <Search onLoadIngredients={filteredIngredientsHandler} />
         <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler} />
         {/* Need to add list here! */}
       </section>
--- src/components/Ingredients/Search.js
+++ src/components/Ingredients/Search.js
@@ -1,15 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import Card from '../UI/Card';
 import './Search.css';
 
 const Search = React.memo(props => {
+  const { onLoadIngredients } = props;
+  const [enteredFilter, setEnteredFilter] = useState('');
+
+  useEffect(() => {
+    console.log('useEffect');
+    const query = enteredFilter.length === 0 ? '' : `?orderBy="title"&equalTo="${enteredFilter}"`
+    fetch('https://react-hooks-6ee32-default-rtdb.firebaseio.com/ingredients.json' + query)
+    .then(response => response.json())
+    .then(resposeData => {
+      const loadedIngredients = [];
+      for(const key in resposeData) {
+        loadedIngredients.push({
+          id: key,
+          title: resposeData[key].title,
+          amount: resposeData[key].amount,
+        });
+      }
+      onLoadIngredients(loadedIngredients);
+    });
+  }, [enteredFilter, onLoadIngredients])
+
   return (
     <section className="search">
       <Card>
         <div className="search-input">
           <label>Filter by Title</label>
-          <input type="text" />
+          <input
+            type="text"
+            value={enteredFilter}
+            onChange={event => setEnteredFilter(event.target.value)}
+          />
         </div>
       </Card>
     </section>
